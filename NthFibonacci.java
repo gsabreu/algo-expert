53957cd7fa835a7b0ebc67b7feb292f7be017d70
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-03-14 01:44:30 -0300

    Update NthFibonacci.java

--- NthFibonacci.java
+++ NthFibonacci.java
@@ -4,9 +4,9 @@ class NthFibonacci {e
 	public static int getNthFib(int n){
 		int[] lastTwo = {0, 1};
 		int counter = 3;
-		while (counter <= n {
+		while (counter <= n) {
 			int nextFib = lastTwo[0] + lastTwo[1];
-			lastTwo[0] = lasTwo[1];
+			lastTwo[0] = lastTwo[1];
 			lastTwo[1] = nextFib;
 			counter ++;
 		}
