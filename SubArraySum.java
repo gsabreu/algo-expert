5497c0e9bd71c6c2fe78366924ff706106f21448
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-03-20 00:31:34 -0300

    Update SubArraySum.java

--- SubArraySum.java
+++ SubArraySum.java
@@ -2,10 +2,9 @@ public class SubArraySum {
     public int subarraySum(int[] nums, int k) {
         int count = 0;
         for (int start = 0; start < nums.length; start++) {
-            for (int end = start + 1; end <= nums.length; end++) {
-                int sum = 0;
-                for (int i = start; i < end; i++)
-                    sum += nums[i];
+            int sum=0;
+            for (int end = start; end < nums.length; end++) {
+                sum+=nums[end];
                 if (sum == k)
                     count++;
             }
